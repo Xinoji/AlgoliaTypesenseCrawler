1fbb3fdcf09338cb396b3cc2b72d6e11321aa933
DvidCastill0 <dviddlccastillo@gmail.com>
2023-11-25 10:50:18 -0600

    Was added more parameters to the function generateHeaders

--- Main.py
+++ Main.py
@@ -15,12 +15,14 @@ def generate_random_headers():
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
     ]
 
     platforms = ["Windows", "Linux", "Mac"]
 
     retHeaders = {
-        "Referer": "https://example.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.8",
         "Sec-Ch-Ua": f"\"{random.choice(user_agents)}\"",
         "Sec-Ch-Ua-Mobile": f"?{random.randint(0, 1)}",
         "Sec-Ch-Ua-Platform": f"\"{random.choice(platforms)}\"",
